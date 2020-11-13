6571409733d467e59faa9d452e1720f3aece3726
phatdroid94 <phatdroid94@gmail.com>
2020-11-13 21:24:32 +0700

    xu ly checkbox

--- app/src/main/java/com/example/progressbarandseekbar05102020/MainActivity.java
+++ app/src/main/java/com/example/progressbarandseekbar05102020/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
@@ -12,7 +14,7 @@ import com.example.progressbarandseekbar05102020.databinding.ActivityMainBinding
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
 
 
     ActivityMainBinding mBinding;
@@ -30,7 +32,14 @@ public class MainActivity extends AppCompatActivity {
         // Task 4 : Cộng điểm khi chiến thắng vào báo khi thua cuộc
 
         init();
+        validCheckbox();
         randomSeekbar();
+    }
+
+    private void validCheckbox() {
+        mBinding.checkboxOne.setOnCheckedChangeListener(this);
+        mBinding.checkboxTwo.setOnCheckedChangeListener(this);
+        mBinding.checkboxThree.setOnCheckedChangeListener(this);
     }
 
     private void init() {
@@ -38,20 +47,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void randomSeekbar() {
+        disableView(mBinding.seekbarOne);
+        disableView(mBinding.seekbarTwo);
+        disableView(mBinding.seekbarThree);
         mCountTimer = new CountDownTimer(1100 , 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 if (millisUntilFinished / 1000 > 0){
                     if (mBinding.seekbarOne.getProgress() >= 100){
                         this.cancel();
+                        enableView(mBinding.seekbarOne);
+                        enableView(mBinding.seekbarTwo);
+                        enableView(mBinding.seekbarThree);
                     } else if (mBinding.seekbarTwo.getProgress() >= 100){
                         this.cancel();
+                        enableView(mBinding.seekbarOne);
+                        enableView(mBinding.seekbarTwo);
+                        enableView(mBinding.seekbarThree);
                     } else if (mBinding.seekbarThree.getProgress() >= 100){
                         this.cancel();
+                        enableView(mBinding.seekbarOne);
+                        enableView(mBinding.seekbarTwo);
+                        enableView(mBinding.seekbarThree);
                     }else{
                         mBinding.seekbarOne.setProgress(mBinding.seekbarOne.getProgress() + mRandom.nextInt(10));
                         mBinding.seekbarTwo.setProgress(mBinding.seekbarTwo.getProgress() + mRandom.nextInt(10));
                         mBinding.seekbarThree.setProgress(mBinding.seekbarThree.getProgress() + mRandom.nextInt(10));
+
                     }
 
                 }
@@ -69,5 +91,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         mCountTimer.cancel();
         super.onStop();
+    }
+    private void enableView(View view){
+        view.setEnabled(true);
+    }
+    private void disableView(View view){
+        view.setEnabled(false);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case R.id.checkboxOne :
+                if (isChecked){
+                    mBinding.checkboxTwo.setChecked(false);
+                    mBinding.checkboxThree.setChecked(false);
+                }
+                break;
+            case R.id.checkboxTwo :
+                if (isChecked){
+                    mBinding.checkboxOne.setChecked(false);
+                    mBinding.checkboxThree.setChecked(false);
+                }
+                break;
+            case R.id.checkboxThree :
+                if (isChecked){
+                    mBinding.checkboxOne.setChecked(false);
+                    mBinding.checkboxTwo.setChecked(false);
+                }
+                break;
+        }
     }
 }
